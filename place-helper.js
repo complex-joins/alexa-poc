9045196822aa578df691cd3f2319e95dfc04c435
Christopher Pruijsen <christopher.pruijsen@gmail.com>
2016-08-05 20:41:33 -0700

    clean up

--- place-helper.js
+++ place-helper.js
@@ -1,38 +1,27 @@
 var placesCall = function(place) {
   var key = 'AIzaSyCHsQMx-gpiPsKxiKd9hhtEdR_GagDRHuw';
-  var url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${place}&key=${key}`;
+  var url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${place}&location=37.76999,-122.44696&radius=500&key=${key}`;
 
-  fetch(url).then(function(res) {
+  fetch(url).then( function(res) {
     return res.json();
-  }).then(function(data) {
-    console.log('success textsearch', data.results[0].geometry.location.lat, data.results[0].geometry.location.lng);
-  }).catch(function(err) {
-    console.log('err places textsearch', err);
+  }).then( function(data) {
+    console.log('places result', data);
+    var place_id = data.predictions[0].place_id;
+    var detailURL = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${key}`;
+
+    fetch(detailURL).then( function(res) {
+      return res.json();
+    }).then( function(data) {
+      console.log('lat place detail', data.result.geometry.location.lat);
+      console.log('lng place detail', data.result.geometry.location.lng);
+    }).catch( function(err) {
+      console.log('error on place detail', err);
+    });
+
+  }).catch(function(err){
+    console.log('err in places', err);
   });
 
-  // var url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${place}&location=37.76999,-122.44696&radius=500&key=${key}`;
-  // console.log('placescall');
-  // fetch(url).then( function(res) {
-  //   return res.json(); // already returns json?
-  // }).then( function(data) {
-  //   console.log('places result', data);
-  //   var place_id = data.predictions[0].place_id;
-  //   var detailURL = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${key}`;
-  //
-  //   fetch(detailURL).then( function(res) {
-  //     return res.json();
-  //   }).then( function(data) {
-  //     console.log('lat place detail', data.result.geometry.location.lat);
-  //     console.log('lng place detail', data.result.geometry.location.lng);
-  //   }).catch( function(err) {
-  //     console.log('error on place detail', err);
-  //   });
-  //
-  // }).catch(function(err){
-  //   console.log('err in places', err);
-  // });
 };
 
-/*
-the places returned from the above are very different (the autocomplete API call gives a much wider range of 'local' options -- probably a better bet for UX. although it 100% requires the user to OK the given destination before we can grab the lat / lng )
-*/
+module.exports = placesCall;
--- ride-helper.js
+++ ride-helper.js
@@ -1,42 +1,10 @@
 var fetch = require('node-fetch');
+var placesCall = require('place-helper'); // invoked as placesCall();
 
-var placesCall = function(place) {
-  var key = 'AIzaSyCHsQMx-gpiPsKxiKd9hhtEdR_GagDRHuw';
-  var url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${place}&key=${key}`;
-
-  fetch(url).then(function(res) {
-    return res.json();
-  }).then(function(data) {
-    console.log('success textsearch', data.results[0].geometry.location.lat, data.results[0].geometry.location.lng);
-  }).catch(function(err) {
-    console.log('err places textsearch', err);
-  });
-
-  // var url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${place}&location=37.76999,-122.44696&radius=500&key=${key}`;
-  // console.log('placescall');
-  // fetch(url).then( function(res) {
-  //   return res.json(); // already returns json?
-  // }).then( function(data) {
-  //   console.log('places result', data);
-  //   var place_id = data.predictions[0].place_id;
-  //   var detailURL = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${place_id}&key=${key}`;
-  //
-  //   fetch(detailURL).then( function(res) {
-  //     return res.json();
-  //   }).then( function(data) {
-  //     console.log('lat place detail', data.result.geometry.location.lat);
-  //     console.log('lng place detail', data.result.geometry.location.lng);
-  //   }).catch( function(err) {
-  //     console.log('error on place detail', err);
-  //   });
-  //
-  // }).catch(function(err){
-  //   console.log('err in places', err);
-  // });
-};
+// NOTE: refactor could be that one first does the placesCall in index.js or here, and on return of coordinates fire the getEstimate.
 
 var getEstimate = function(requestType, start, dest) {
-  // take input 'requestType' that will either be fastest or cheapest as string
+  // take input 'requestType' that will either be 'fastest' or 'cheapest'
   var uberURL = 'https://api.uber.com/v1/';
   var lyftURL = 'https://api.lyft.com/v1/';
 
@@ -62,7 +30,6 @@ var getEstimate = function(requestType, start, dest) {
 
   // TODO: refactor index.js to pass a requestType based on user intent
   // return alexa speech based on comparison result
-  // start google places experiment based on alexa custom slot
 
   var firstResult = null;
   var winner = null;
@@ -138,113 +105,3 @@ module.exports = {
   placesCall: placesCall,
   getEstimate: getEstimate
 };
-
-// fetch(url, {
-//   method: 'GET',
-//   body: {},
-//   method: cors
-// }).then().catch()
-
-// google places
-/*
-https://developers.google.com/places/web-service/autocomplete
-or
-https://developers.google.com/maps/documentation/javascript/places-autocomplete
-
-OR
-https://smartystreets.com/features
-"Get 250 free US address lookups per month, forever."
-*/
-
-// uber
-
-/*
-
-curl -H 'Authorization: Token pG-f76yk_TFCTMHtYHhY7xUfLVwmt9u-l4gmgiHE' \
-'https://api.uber.com/v1/products?latitude=37.7752135&longitude=-122.4369302'
-
-POOLSF - "product_id": "26546650-e557-4a7b-86e7-6a3942445247",
-XSF -  "product_id": "a1111c8c-c720-46c3-8534-2fcdd730040d",
-
-**  For example, uberX in San Francisco will have a different product_id than uberX in Los Angeles.
-
-/v1/products/
-
-parameters:
-latitude
-longitude
-
-return:
-
-{
-  "products": [
-    {
- "display_name": "POOL", --> important
- "product_id": "26546650-e557-4a7b-86e7-6a3942445247",
- },{//other}, {//other} ]}
-
- // could include an 'info about type' , but not essential.
-
-https://api.uber.com/v1/estimates/time
-https://developer.uber.com/docs/rides/api/v1-estimates-time
-
-https://api.uber.com/v1/estimates/price
-https://developer.uber.com/docs/rides/api/v1-estimates-price
-
-format -->
-
-    url: "https://api.uber.com/v1/estimates/price",
-    headers: {
-        Authorization: "Token " + uberServerToken
-    }
-
-    body: {
-        start_latitude: user_latitude,
-        start_longitude: user_longitude,
-        end_latitude: dest_lat,
-        end_longitude: dest_long
-    }
-}
-
-*/
-
-// lyft
-
-/*
-
-https://developer.lyft.com/docs/authentication
-// need to get an auth token for testing.
-
-'https://api.lyft.com/v1/ridetypes'
-
-response:
-{
-  "ride_types": [
-    {
-      "display_name": "Lyft Line",
-      "ride_type": "lyft_line", }, {//other}, {//other} ] }
-
-parameters:
-lat	/> the user’s current latitude
-lng	/> the user’s current longitude
-ride_type	/> the specific ride type
-
-'https://api.lyft.com/v1/eta'
-https://developer.lyft.com/docs/availability-etas
-
-parameters:
-lat	/> the user’s current latitude
-lng	/> the user’s current longitude
-ride_type /> any of the ridetypes returned by the ridetypes endpoint
-
-'https://api.lyft.com/v1/cost'
-https://developer.lyft.com/docs/availability-cost
-
-parameters:
-start_lat	\> origin latitude
-start_lng	\> origin longitude
-end_lat	\>	the destination latitude
-end_lng	\>	the destination longitude
-ride_type	\>	any of the ridetypes returned by the ridetypes endpoint
-
-*/
